f72da0a0a066fa755b54b6f98436b39bd7f8e5a0
Isreal <in.visual7@gmail.com>
2019-12-22 19:38:34 -0500

    ✍

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# canvas-vue
+# Tokei - A Multi-Clock
 
-## Project setup
-```
-yarn install
-```
+> A Multi-Clock. Built with Vue.js x Vuesax.
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+> 2nd in a series of practice projects. 
 
-### Compiles and minifies for production
-```
-yarn build
-```
+### What's it do? - To-Do
 
-### Lints and fixes files
-```
-yarn lint
-```
+- [x] Digital Display
+- [x] Pomodoro 
+- [ ] Alarms maybe??
+- [ ] Complete Responsivity - To bes tested...
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/1e89f57c-185a-45e3-a537-646d97b9a10e/deploy-status)](https://app.netlify.com/sites/dentaku/deploys)
+
+[Link to Project](https://dentaku.netlify.com)
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install || yarn
+
+# serve with hot reload at localhost:8080
+npm run dev || yarn dev
+
+# build for production with minification
+npm run build || yarn build
+
+```
---
+++ dist/precache-manifest.c96345c58db65fcf4b006d597a131c9c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b1c426edc0d537f622",
+    "url": "/css/app.2ec99bd6.css"
+  },
+  {
+    "revision": "41958227f9037dd11ebf",
+    "url": "/css/chunk-vendors.d79022d0.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/fonts/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "eee8a88895067307b9e93bf962161c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4af2b9c986e3523323ba",
+    "url": "/js/about.31392ebc.js"
+  },
+  {
+    "revision": "d9b1c426edc0d537f622",
+    "url": "/js/app.253379c2.js"
+  },
+  {
+    "revision": "41958227f9037dd11ebf",
+    "url": "/js/chunk-vendors.c8d37964.js"
+  },
+  {
+    "revision": "bd15ab99524bdeb6f4eca736f18ceec6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
